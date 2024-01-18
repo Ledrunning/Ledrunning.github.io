db65e56ea2ee25eb2de5b7c0bb39935623253ab7
Ledrunning <ledrunner@hotmail.com>
2024-01-18 23:20:04 +0100

    Added js files and some logic

---
+++ data.js
@@ -0,0 +1,81 @@
+export const bio = [
+    `With over 10 years of expertise, I am a seasoned software
+    developer skilled in C# .NET frameworks, web development,
+    design microservices, and desktop applications across GTK+,
+    Windows Forms, WPF, and UWP. Transitioning from a solid
+    foundation in electronics engineering focused on embedded
+    systems, I merge interdisciplinary knowledge to offer unique
+    solutions. I bring a blend of cutting-edge techniques and
+    robust experience to drive innovation and efficiency to a
+    dynamic tech environment.`,
+];
+
+export const skills = [
+    {
+        title: "Languages",
+        skillName: "C, C# TypeScript",
+        color: "1",
+        percentage: "80",
+    },
+    {
+        title: "Frameworks/Libraries",
+        skillName: `.NET Core 3.1, 5, 6, 7, .NET Framework
+                    4.6, 4.7, 4.8, ASP.NET Core, Vue.js`,
+        color: "6",
+        percentage: "70",
+    },
+    {
+        title: "Design",
+        skillName: "HTML, XAML, GTK, Bootstrap",
+        color: "4",
+        percentage: "70",
+    },
+    {
+        title: "Databases:",
+        skillName: "SQLite, MongoDB, MSSQL, Cosmos DB",
+        color: "4",
+        percentage: "70",
+    },
+    {
+        title: "ORMs and Database Tools:",
+        skillName: "Dapper, ADO.NET, FluentMigrator, Fluent API, Entity Framework",
+        color: "4",
+        percentage: "70",
+    },
+    {
+        title: "Tools & Platforms:",
+        skillName: "Azure, Docker, Git, Jira, AppVeyor, Confluence, YouTrack",
+        color: "4",
+        percentage: "70",
+    },
+    {
+        title: "Principles & Practices:",
+        skillName: "SOLID, OOP, DRY, CI/CD",
+        color: "4",
+        percentage: "70",
+    },
+    {
+        title: "Version Control",
+        skillName: "GitHub, JIRA, Bitbucket",
+        color: "7",
+        percentage: "70",
+    },
+    {
+        title: "Unit Test",
+        skillName: " MSTest, Nunit, Moq",
+        color: "7",
+        percentage: "70",
+    },
+    {
+        title: "Tools",
+        skillName: "Postman, NServiceBus Analyzer, Chrome DevTools",
+        color: "3",
+        percentage: "80",
+    },
+    {
+        title: "IDE",
+        skillName: "VS Code, Visual Studio",
+        color: "6",
+        percentage: "70",
+    },
+];
