6d3dce7bb812a165976938c85c5c0bfca779747c
Ledrunning <ledrunner@hotmail.com>
2024-03-01 23:15:24 +0100

    Fixed issue with projects

--- data.js
+++ data.js
@@ -10,22 +10,22 @@ export const bio = [
 export const skills = [
     {
         title: "Languages",
-        skillName: "C, C# TypeScript",
+        skillName: "C, C#, TypeScript",
         color: "1",
-        percentage: "80",
+        percentage: "90",
     },
     {
         title: "Frameworks/Libraries",
         skillName: `.NET Core 3.1, 5, 6, 7, .NET Framework
                     4.6, 4.7, 4.8, ASP.NET Core, Vue.js`,
         color: "6",
-        percentage: "70",
+        percentage: "85",
     },
     {
         title: "Design",
         skillName: "HTML, XAML, GTK, Bootstrap",
         color: "4",
-        percentage: "70",
+        percentage: "60",
     },
     {
         title: "Databases:",
@@ -37,25 +37,25 @@ export const skills = [
         title: "ORMs and Database Tools:",
         skillName: "Dapper, ADO.NET, FluentMigrator, Fluent API, Entity Framework",
         color: "4",
-        percentage: "70",
+        percentage: "80",
     },
     {
         title: "Tools & Platforms:",
         skillName: "Azure, Docker, Git, Jira, AppVeyor, Confluence, YouTrack",
         color: "4",
-        percentage: "70",
+        percentage: "85",
     },
     {
         title: "Principles & Practices:",
         skillName: "SOLID, OOP, DRY, CI/CD",
         color: "4",
-        percentage: "70",
+        percentage: "90",
     },
     {
         title: "Version Control",
         skillName: "GitHub, JIRA, Bitbucket",
         color: "7",
-        percentage: "70",
+        percentage: "90",
     },
     {
         title: "Unit Test",
@@ -71,9 +71,9 @@ export const skills = [
     },
     {
         title: "IDE",
-        skillName: "VS Code, Visual Studio",
+        skillName: "VS Code, Visual Studio, ReSharper",
         color: "6",
-        percentage: "70",
+        percentage: "100",
     },
 ];
 
@@ -98,7 +98,127 @@ export const education = [
         duration: "2003 - 2008",
         subtitle: "Sevastopol State Technical University",
         details: [],
-        tags: ["Physics", "Circuit design", "Power electronics", "PCB Route", "Mathematics"],
+        tags: [
+            "Physics",
+            "Circuit design",
+            "Power electronics",
+            "PCB Route",
+            "Mathematics",
+        ],
         icon: "book",
+    },
+];
+
+export const projects = {
+    webProjects: [
+        {
+            projectName: "",
+            image: "",
+            summary: "",
+            preview: "",
+            techStack: ["", "", "", ""],
+        },
+        {
+            projectName: "",
+            image: "",
+            summary: "",
+            preview: "",
+            techStack: ["", "", "", ""],
+        },
+    ],
+    softwareProjects: [
+        {
+            projectName: "",
+            image: "",
+            summary: "",
+            preview: "",
+            techStack: ["", ""],
+        },
+    ],
+    androidProjects: [
+        {
+            projectName: "",
+            image: "",
+            summary: "",
+            preview: "",
+            techStack: ["", "", ""],
+        },
+    ],
+};
+
+export const experience = [
+    {
+        title: "Boob Company",
+        duration: "September 2022 - Present",
+        subtitle: "Software Engineer",
+        details: ["Working in support and escalation team."],
+        tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
+        icon: "truck ",
+    },
+    {
+        title: "Bustas",
+        duration: "September 2022 - Present",
+        subtitle: "Software Engineer",
+        details: ["Working in support and escalation team."],
+        tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
+        icon: "heartbeat ",
+    },
+];
+
+export const footer = [
+    {
+        label: "Dev Profiles",
+        data: [
+            {
+                text: "Stackoverflow",
+                link: "https://stackoverflow.com/users/8070900/jdo",
+            },
+            {
+                text: "GitHub",
+                link: "https://github.com/Ledrunning",
+            },
+            {
+                text: "Reddit",
+                link: "https://www.reddit.com/user/Ledrunnin/",
+            },
+        ],
+    },
+    {
+        label: "Resources",
+        data: [
+            {
+                text: "Enable Dark/Light Mode",
+                func: "enableDarkMode()",
+            },
+            {
+                text: "Print this page",
+                func: "window.print()",
+            },
+            {
+                text: "Clone this page",
+                link: "https://github.com/Ledrunning/Ledrunning.github.io",
+            },
+        ],
+    },
+    {
+        label: "Social Profiles",
+        data: [
+            {
+                text: "Linkedin",
+                link: "https://www.linkedin.com/in/osman-mazinov-lennarovich/",
+            },
+            {
+                text: "Xing",
+                link: "https://www.xing.com/profile/Osman_Mazinov/",
+            },
+        ],
+    },
+    {
+        label: "copyright-text",
+        data: [
+            "Made with &hearts; by Vinay Somawat.",
+            "&copy; No Copyrights. Feel free to use this template.",
+            "The template was taken from Vinay Somawat <a href='https://github.com/vinaysomawat/vinaysomawat.github.io'>link text</a>",
+        ],
     },
 ];
