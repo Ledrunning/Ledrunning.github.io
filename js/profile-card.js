2be92ba1fa45da9564233da5a8c77e08e503c2ea
Ledrunning <ledrunner@hotmail.com>
2025-04-09 10:56:23 +0200

    Added social media and icons for them

--- js/profile-card.js
+++ js/profile-card.js
@@ -4,49 +4,40 @@ window.addEventListener("DOMContentLoaded", async function () {
         return resp.json();
     }
 
+    // StackOverflow
     document.querySelectorAll(".stack-card").forEach(async function (el) {
         const userId = el.getAttribute("user-id");
-
         const response = await get(`https://api.stackexchange.com/2.2/users/${userId}?site=stackoverflow`);
         const user = response.items[0];
         const { profile_image, website_url, link, display_name, reputation, user_id } = user;
         const { gold, silver, bronze } = user.badge_counts;
 
         el.innerHTML = `
-        <div style="font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji; border-radius: 6px; line-height: 1.5; padding: 16px; font-size: 14px; color: #24292e; background-color: #7fc7af;">
+            <div style="font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif;">
                 <div style="display: flex; align-items: center;">
-                    <img style="width: 48px; height: 48px; border-radius: 50%" src="${profile_image}" alt="Profile image"></img>
+                    <img style="width: 48px; height: 48px; border-radius: 50%" src="${profile_image}" alt="Profile image">
                     <div style="display: flex; flex-direction: column; margin-left: 12px">
-                        <span style="font-weight: 500; color: #black; font-size: 18px">
+                        <span style="font-weight: 500; color: #000;">
                             <a style="text-decoration: none; color: inherit;" target="_blank" href="${website_url && link}">
                                 ${display_name}
                             </a>
                         </span>
-                        <span style="font-weight: 400; color: #586069; font-size: 12px">
-                            @${link.replace("https://", '').replace(`/users/${user_id}`,'')}
+                        <span style="font-weight: 400; color: #586069;">
+                            @${link.replace("https://", '').replace(`/users/${user_id}`, '')}
                         </span>
                     </div>
                 </div>
-
-                <div style="margin-top: 12px; display: flex; justify-content: space-evenly; align-items: center; ">
-                    <div style="margin-top: -4px">
-                        <span style="font-size: 10px; font-weight: 500; color: #586069;">
-                            REPUTATIONS
-                        </span>
-                        <div style="font-weight: 400; color: #211F1F; font-size: 12px; margin-top: 2px">
-                            <span style="font-weight: 600; color: #211F1F; font-size: 32px; line-height: 1;">
-                                ${reputation}
-                            </span>
+                <div style="margin-top: 12px; display: flex; justify-content: space-evenly;">
+                    <div>
+                        <span style="font-size: 10px;">Reputation</span>
+                        <div style="font-size: 32px; line-height: 1;">
+                            ${reputation}
                         </div>
                     </div>
-                    <div style="margin-top: -4px">
-                        <span style="font-size: 10px; font-weight: 500; color: #586069;">
-                            BADGES
-                        </span>
-                        <div style="font-weight: 400; color: #211F1F; font-size: 12px; margin-top: 2px">
-                            <span style="font-weight: 600; color: #211F1F; font-size: 32px; line-height: 1;">
-                                ${Number(gold) + Number(silver) + Number(bronze)}
-                            </span>
+                    <div>
+                        <span style="font-size: 10px;">Badges</span>
+                        <div style="font-size: 32px; line-height: 1;">
+                            ${Number(gold) + Number(silver) + Number(bronze)}
                         </div>
                     </div>
                 </div>
@@ -54,51 +45,110 @@ window.addEventListener("DOMContentLoaded", async function () {
         `;
     });
 
+    // GitHub
     document.querySelectorAll(".github-card").forEach(async function (el) {
         const username = el.getAttribute("username");
-
         const response = await get(`https://api.github.com/users/${username}`);
         const { name, avatar_url, public_repos, followers, html_url, following } = response;
 
         el.innerHTML = `
-            <div style="font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji; border-radius: 6px; line-height: 1.5; padding: 16px; font-size: 14px; color: #24292e; background-color: #7fc7af;">
-                <div style="display: flex; align-items: center; margin-top: -4px">
-                    <img style="width: 48px; height: 48px; border-radius: 50%" src="${avatar_url}" alt="Profile image"></img>
+            <div style="font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif;">
+                <div style="display: flex; align-items: center;">
+                    <img style="width: 48px; height: 48px; border-radius: 50%" src="${avatar_url}" alt="Profile image">
                     <div style="display: flex; flex-direction: column; margin-left: 12px">
-                        <span style="font-weight: 500; color: #black; font-size: 18px">
+                        <span style="font-weight: 500; color: #000;">
                             <a style="text-decoration: none; color: inherit;" target="_blank" href="${html_url}">
                                 ${name}
                             </a>
                         </span>
-                        <span style="font-weight: 400; color: #586069; font-size: 12px">
+                        <span style="font-weight: 400; color: #586069;">
                             @${html_url.replace('https://', '')}
                         </span>
                     </div>
                 </div>
-
-                <div style="margin-top: 12px; display: flex; justify-content: space-evenly; align-items: center; ">
-                    <div style="display: flex; flex-direction: column;">
-                        <span style="font-size: 10px; font-weight: 500; color: #586069;">
-                            REPOSITORIES
-                        </span>
-                        <span style="font-weight: 600; color: #211F1F; font-size: 32px; line-height: 1">
+                <div style="margin-top: 12px; display: flex; justify-content: space-evenly;">
+                    <div>
+                        <span style="font-size: 10px;">Repositories</span>
+                        <div style="font-size: 32px; line-height: 1;">
                             ${public_repos}
-                        </span>
+                        </div>
                     </div>
-                    <div style="display: flex; flex-direction: column;">
-                        <span style="font-size: 10px; font-weight: 500; color: #586069;">
-                            FOLLOWERS
-                        </span>
-                        <span style="font-weight: 600; color: #211F1F; font-size: 32px; line-height: 1">
+                    <div>
+                        <span style="font-size: 10px;">Followers</span>
+                        <div style="font-size: 32px; line-height: 1;">
                             ${followers}
+                        </div>
+                    </div>
+                    <div>
+                        <span style="font-size: 10px;">Following</span>
+                        <div style="font-size: 32px; line-height: 1;">
+                            ${following}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    // Instagram
+    document.querySelectorAll(".instagram-card").forEach(async function (el) {
+        const username = el.getAttribute("username");
+        const accessToken = 'YOUR_INSTAGRAM_ACCESS_TOKEN'; // Здесь нужно использовать ваш токен доступа
+        const response = await get(`https://graph.instagram.com/${username}?fields=id,username,media_count,followers_count,follows_count,profile_picture_url&access_token=${accessToken}`);
+        const { username: instaUsername, media_count, followers_count, follows_count, profile_picture_url } = response;
+
+        el.innerHTML = `
+            <div style="font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif;">
+                <div style="display: flex; align-items: center;">
+                    <img style="width: 48px; height: 48px; border-radius: 50%" src="${profile_picture_url}" alt="Profile image">
+                    <div style="display: flex; flex-direction: column; margin-left: 12px">
+                        <span style="font-weight: 500; color: #000;">
+                            <a style="text-decoration: none; color: inherit;" target="_blank" href="https://www.instagram.com/${instaUsername}/">
+                                ${instaUsername}
+                            </a>
                         </span>
                     </div>
-                    <div style="display: flex; flex-direction: column;">
-                        <span style="font-size: 10px; font-weight: 500; color: #586069;">
-                            FOLLOWING
-                        </span>
-                        <span style="font-weight: 600; color: #211F1F; font-size: 32px; line-height: 1">
-                            ${following}
+                </div>
+                <div style="margin-top: 12px; display: flex; justify-content: space-evenly;">
+                    <div>
+                        <span style="font-size: 10px;">Media</span>
+                        <div style="font-size: 32px; line-height: 1;">
+                            ${media_count}
+                        </div>
+                    </div>
+                    <div>
+                        <span style="font-size: 10px;">Followers</span>
+                        <div style="font-size: 32px; line-height: 1;">
+                            ${followers_count}
+                        </div>
+                    </div>
+                    <div>
+                        <span style="font-size: 10px;">Following</span>
+                        <div style="font-size: 32px; line-height: 1;">
+                            ${follows_count}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    // LinkedIn
+    document.querySelectorAll(".linkedin-card").forEach(async function (el) {
+        const username = el.getAttribute("username");
+        const accessToken = 'YOUR_LINKEDIN_ACCESS_TOKEN'; // Здесь нужно использовать ваш токен доступа
+        const response = await get(`https://api.linkedin.com/v2/me?oauth2_access_token=${accessToken}`);
+        const { firstName, lastName, profilePicture, publicProfileUrl } = response;
+
+        el.innerHTML = `
+            <div style="font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif;">
+                <div style="display: flex; align-items: center;">
+                    <img style="width: 48px; height: 48px; border-radius: 50%" src="${profilePicture.displayImage~.elements[0].identifiers[0].identifier}" alt="Profile image">
+                    <div style="display: flex; flex-direction: column; margin-left: 12px">
+                        <span style="font-weight: 500; color: #000;">
+                            <a style="text-decoration: none; color: inherit;" target="_blank" href="${publicProfileUrl}">
+                                ${firstName.localized.en_US} ${lastName.localized.en_US}
+                            </a>
                         </span>
                     </div>
                 </div>
